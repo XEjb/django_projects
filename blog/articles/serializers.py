df250564dee06db75e757a3ab9ac27370ced3de7
Vladislav Artamonenko <54907042+XEjb@users.noreply.github.com>
2022-07-21 19:49:35 +0300

    Delete blog directory

--- blog/articles/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Author
-
-
-class AuthorSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Author
-        fields = ['name', 'age', 'uuid']
